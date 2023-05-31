e20e75d4b28a753cf6851cbadad67bb78f25bb01
rosenfence <rosenfence@gmail.com>
2023-05-31 22:31:57 +0900

    rosenfence / 230531

--- rosenfence/part2/app/list/ListItem.js
+++ rosenfence/part2/app/list/ListItem.js
@@ -3,27 +3,32 @@ import React from 'react';
 import Link from 'next/link';
 
 const ListItem = ({ lists }) => {
-  const handleDelete = (list) => {
-    fetch('/api/delete', { method: 'DELETE', body: list._id }).then(() => {
-      console.log('삭제됨');
-    });
+  const handleDelete = (list, e) => {
+    fetch('/api/delete', { method: 'DELETE', body: list._id })
+      .then((res) => {
+        res.json();
+      })
+      .then(() => {
+        e.target.parentElement.style.opacity = 0;
+        setTimeout(() => {
+          e.target.parentElement.style.display = 'none';
+        }, 1000);
+      });
   };
 
   return (
     <div>
       {lists.map((list, i) => {
         return (
-          <div key={i}>
-            <div className='list-item'>
-              <Link className='link' href={`/detail/${list._id}`}>
-                <h4>{list.title}</h4>
-                <p>{list.content}</p>
-              </Link>
-              <Link className='link' href={`/edit/${list._id}`}>
-                수정
-              </Link>
-              <span onClick={() => handleDelete(list)}>삭제</span>
-            </div>
+          <div className='list-item' key={i}>
+            <Link className='link' href={`/detail/${list._id}`}>
+              <h4>{list.title}</h4>
+              <p>{list.content}</p>
+            </Link>
+            <Link className='link' href={`/edit/${list._id}`}>
+              수정
+            </Link>
+            <span onClick={(e) => handleDelete(list, e)}>삭제</span>
           </div>
         );
       })}
