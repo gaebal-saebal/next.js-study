b1c288faa8766b964d6e9f12b28f8d183ee4fbd6
rosenfence <rosenfence@gmail.com>
2023-06-07 02:36:52 +0900

    rosenfence / 230605(3)

--- rosenfence/part2/app/detail/[id]/Comment.js
+++ rosenfence/part2/app/detail/[id]/Comment.js
@@ -15,40 +15,48 @@ const Comment = ({ postId }) => {
       });
   }, []);
 
-  const handleClick = () => {
-    fetch('/api/comment/new', {
-      method: 'POST',
-      body: JSON.stringify({ content: comment, parent: postId }),
-    })
-      .then((res) => {
-        return res.json();
+  const handleClick = (e) => {
+    if (comment.length > 0) {
+      fetch('/api/comment/new', {
+        method: 'POST',
+        body: JSON.stringify({ content: comment, parent: postId }),
       })
-      .then((data) => {
-        setLists(data);
-      })
-      .catch(() => {
-        console.log('오류발생');
-      });
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setLists(data);
+          e.target.parentElement.children[1].value = '';
+          setComment('');
+        })
+        .catch(() => {
+          console.log('오류발생');
+        });
+    } else {
+      alert('댓글에 뭐라도 적어주세요');
+    }
   };
 
   return (
     <div>
       <div>
-        {lists.map((list, i) => {
-          return (
-            <div key={i}>
-              <span>{list.content}</span>
-              <span>{list.author}</span>
-            </div>
-          );
-        })}
+        {lists.length > 0
+          ? lists.map((list, i) => {
+              return (
+                <div key={i}>
+                  <span>{list.content}</span>
+                  <span>{list.author}</span>
+                </div>
+              );
+            })
+          : null}
       </div>
       <input
         onChange={(e) => {
           setComment(e.target.value);
         }}
       />
-      <button onClick={handleClick}>댓글전송</button>
+      <button onClick={(e) => handleClick(e)}>댓글전송</button>
     </div>
   );
 };
